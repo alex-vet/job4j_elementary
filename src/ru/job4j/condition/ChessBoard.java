8b79c875bfc839088e083b4272af7e19b63c81cc
alex-vet <alex157@inbox.ru>
2020-10-09 15:28:53 +0400

    4.3.6. Math.abs [#383164]

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+        int rsl = 0;
+        if ((x1 >= 0 && x1 < 8) && (x2 >= 0 && x2 < 8)  &&  (y1 >= 0 && y1 < 8) && (y2 >= 0 && y2 < 8)) {
+                rsl = Math.abs(x2 - x1);
+                rsl = rsl == Math.abs(y2 - y1) ?  rsl : 0;
+        }
+        return rsl;
+    }
+}
