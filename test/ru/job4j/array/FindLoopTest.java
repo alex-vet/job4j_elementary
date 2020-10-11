4ce1c1f741269283e10164d595520109aa6443e6
alex-vet <alex157@inbox.ru>
2020-10-11 19:58:30 +0400

    6.1. Классический поиск перебором. [#383171]

---
+++ test/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] input = {5, 4, 3, 2};
+        int value = 5;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHas9ThenMinus1() {
+        int[] input = {8, 7, 6, 5, 4, 3, 2, 1};
+        int value = 9;
+        int result = FindLoop.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+}
